9d4a2da6b2e7a481f41e03dc0ef1feac1d2ac204
Aysun02 <104448452+Aysun02@users.noreply.github.com>
2022-10-05 17:25:19 +0400

    Add files via upload

---
+++ WebApplication2/WebApplication2/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Name = "Aysun";
+            ViewBag.Surname = "Huseynli";
+            ViewBag.Age = "19";
+            ViewBag.Country = "Azerbaijan";
+            ViewBag.PhoneNumber = "+994506143502";
+            return View();
+        }
+    }
+}
